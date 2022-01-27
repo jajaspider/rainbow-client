901065e3d9f621c707b3122d1c2732070d2aa091
SonaAPI <jaja_spider@naver.com>
2022-01-28 02:35:49 +0900

    feat(#3): Add info method
    정보 - info 명령어를 추가하였습니다.
    캐릭터가 없거나, 문제가있을경우 errorMessage를 return합니다.
    정상인 경우 정보 데이터를 제공합니다.

--- src/service/lostark.js
+++ src/service/lostark.js
@@ -5,6 +5,7 @@ const async = require("async");
 
 async function exec(methodObj, chat, author) {
     let command = _.get(methodObj, "name");
+    let chatLength = chat.split(" ").length;
     switch (command) {
         case "help":
             let maplestoryMethods = await Lostark.find({}).lean();
@@ -23,7 +24,67 @@ async function exec(methodObj, chat, author) {
 
             return {
                 type: "sendChat",
-                    result,
+                result,
+            };
+
+            break;
+        case 'info':
+            let url = null;
+            if (chat == '') {
+                url = `http://192.168.1.142:30003/v0/lostark/info/${encodeURIComponent(author)}`;
+            } else if (chatLength == 1) {
+                url = `http://192.168.1.142:30003/v0/lostark/info/${encodeURIComponent(chat)}`;
+            }
+            let request = await axios.get(url);
+            if (request.status != 200) {
+                return {};
+            }
+            let data = _.get(request, "data");
+            let errorMessage = _.get(data, 'payload.message');
+            if (errorMessage) {
+                return {
+                    type: "sendChat",
+                    result: errorMessage,
+                }
+            }
+
+            let character = _.get(data, 'payload.character');
+            let server = _.get(character, 'server');
+            let nickname = _.get(character, 'nickname');
+            let job = _.get(character, 'job');
+            let fightLevel = _.get(character, 'fightLevel');
+            let itemLevel = _.get(character, 'itemLevel');
+            let attack = _.get(character, 'attack');
+            let health = _.get(character, 'health');
+            let specificList = _.get(character, 'specificList');
+            let guildName = _.get(character, 'guildName');
+            let engraveList = _.get(character, 'engraveList');
+            let cardList = _.get(character, 'cardList');
+
+            let info = `${nickname} - ${server} | ${job}\n`;
+            info += `전투레벨 : ${fightLevel}\n`;
+            info += `아이템레벨 : ${itemLevel}\n`;
+            info += `공격력 : ${attack}\n`;
+            info += `생명력 : ${health}\n`;
+            info += `길드:  : ${guildName}\n\n`;
+
+            for (let specific of specificList) {
+                info += `${specific.specificName}(${specific.specificValue}) `;
+            }
+            info += `\n\n`;
+
+            for (let engrave of engraveList) {
+                info += `${engrave}\n`;
+            }
+            // result += `\n`;
+
+            for (let card of cardList) {
+                info += `\n${card.cardSet} | ${card.cardSetValue}`;
+            }
+
+            return {
+                type: "sendChat",
+                result: info,
             };
 
         default:

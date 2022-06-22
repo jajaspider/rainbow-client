2f1e19b175dbe7742677ac9e68b1612cb89abd47
SonaAPI <jaja_spider@naver.com>
2022-06-23 01:11:31 +0900

    feat: support event command

--- src/service/lostark.js
+++ src/service/lostark.js
@@ -7,6 +7,7 @@ const {
 } = require('../core/eventBridge');
 const COMPRES = "\u200b".repeat(500);
 const imageService = require('./imageService');
+const rainbowUtil = require('../utils');
 
 async function exec(methodObj, chat, nickname, channelId, client) {
     let command = _.get(methodObj, "name");
@@ -180,7 +181,87 @@ async function exec(methodObj, chat, nickname, channelId, client) {
                 client
             });
             break;
+        case "eventList":
+            url = `http://127.0.0.1:30003/v0/lostark/event`
 
+            response = await axios.get(url);
+            if (response.status != 200) {
+                return;
+            }
+
+            responseData = _.get(response, "data");
+            errorMessage = _.get(responseData, 'payload.message');
+            if (errorMessage) {
+                chatEvent.emit('send', {
+                    channelId,
+                    type: 'chat',
+                    data: errorMessage,
+                    client
+                });
+                return;
+                // return {
+                //     type: "sendChat",
+                //     result: errorMessage,
+                // }
+            }
+            let eventList = _.get(responseData, 'payload.events');
+
+            templateArgs = {};
+            for (let i = 0; i < (eventList.length <= 5 ? eventList.length : 5); i += 1) {
+                console.dir(eventList[i]);
+                templateArgs[`event_name_${i + 1}`] = eventList[i].title;
+                let term = (eventList[i].term).replace('이벤트 기간 : ', '')
+                templateArgs[`event_contents_${i + 1}`] = term;
+                templateArgs[`event_image_${i + 1}`] = eventList[i].img_path;
+                templateArgs[`url${i + 1}`] = eventList[i].link;
+            }
+
+            templateId = 78643;
+
+            if (client == 'kakao') {
+                chatEvent.emit('send', {
+                    channelId,
+                    type: 'kakaolink',
+                    data: {
+                        templateId,
+                        templateArgs
+                    },
+                    client
+                });
+            } else if (client == 'discord') {
+                // chatEvent.emit('send', {
+                //     channelId,
+                //     type: 'embed',
+                //     subType: 'maplestoryInfo',
+                //     data: character,
+                //     client
+                // });
+            }
+
+            if (eventList.length > 5) {
+                await rainbowUtil.sleep(1000);
+                let message = `나머지 ${eventList.length - 5}개 이벤트\n${COMPRES}`;
+                for (let i = 5; i < eventList.length; i += 1) {
+                    message += `\n${eventList[i].title}`;
+                    message += `\n${eventList[i].link}`;
+                    message += `\n${eventList[i].term}`;
+                    message += `\n${eventList[i].receive}`;
+                }
+                // 각각의 타입이 정식 지원이되면 해당하는 send는 한개로 합쳐도 상관없음
+                if (client == 'kakao') {
+                    chatEvent.emit('send', {
+                        channelId,
+                        type: 'chat',
+                        data: message,
+                        client
+                    });
+                } else if (client == 'discord') {
+                    // 현재는 skip
+                }
+            }
+
+
+            break;
 
         default:
             break;

053a5d772e7d753aa56d5f6368823aab055149e3
SonaAPI <jaja_spider@naver.com>
2022-09-04 19:22:08 +0900

    feat(#36): Add boss command for manager
    관리자용 보스 관리 명령어가 정의되었습니다.
    보스 추가, 보스 보상 수정이 가능합니다.
    보스 보상 삭제, 보스 삭제는 추후 지원합니다.

--- src/service/manage.js
+++ src/service/manage.js
@@ -1,14 +1,25 @@
 const _ = require("lodash");
 const axios = require("axios");
 const Permission = require("../models/index").Permission;
-const async = require("async");
 const {
     chatEvent
 } = require('../core/eventBridge');
+const path = require('path');
+const fs = require('fs');
+const yaml = require('js-yaml');
+let configPath = path.join(process.cwd(), 'config', 'rainbow.develop.yaml');
+let config = yaml.load(fs.readFileSync(configPath));
 
 async function exec(methodObj, chat, channelId, attachmentId, client) {
     let command = _.get(methodObj, "name");
-    let chatLength = chat.split(" ").length;
+    let reg = /(?:[^\s"]+|"[^"]*")+/g;
+    let chatSplit = chat.match(reg);
+    let chatLength = chatSplit.length;
+    let response = null;
+    let responseData = null;
+    let errorMessage = null;
+    let url = null;
+    let apiEndpoint = `http://${_.get(config, 'site.domain')}:${_.get(config, 'site.port')}/api/v0`;
 
     switch (command) {
         case "roomRegister":
@@ -104,7 +115,256 @@ async function exec(methodObj, chat, channelId, attachmentId, client) {
                 client
             });
             break;
+        case 'registerBoss':
+            if (chatSplit.length != 3) {
+                chatEvent.emit('send', {
+                    channelId,
+                    type: 'chat',
+                    data: "잘못 입력하셨습니다.",
+                    client
+                });
+                return;
+            }
+            let registerPayload = {
+                game: chatSplit[0],
+                name: chatSplit[1],
+                level: chatSplit[2]
+            };
 
+            if (registerPayload.game == '로아' || registerPayload.game == '로스트아크') {
+                registerPayload.game = 'lostark'
+            }
+            else if (registerPayload.game == '메이플' || registerPayload.game == '메이플스토리') {
+                registerPayload.game = 'maplestory'
+            }
+            else {
+                chatEvent.emit('send', {
+                    channelId,
+                    type: 'chat',
+                    data: "잘못 입력하셨습니다.",
+                    client
+                });
+                return;
+            }
+
+            response = await axios.post(`${apiEndpoint}/boss/register`, registerPayload);
+            if (response.status != 200) {
+                return;
+            }
+            responseData = _.get(response, "data");
+            errorMessage = _.get(responseData, 'payload.message');
+            if (errorMessage) {
+                chatEvent.emit('send', {
+                    channelId,
+                    type: 'chat',
+                    data: errorMessage,
+                    client
+                });
+                return;
+            }
+
+            chatEvent.emit('send', {
+                channelId,
+                type: 'chat',
+                data: '등록 완료',
+                client
+            });
+            return;
+            break;
+        case 'editMoney':
+            if (chatSplit.length != 3) {
+                chatEvent.emit('send', {
+                    channelId,
+                    type: 'chat',
+                    data: "잘못 입력하셨습니다.",
+                    client
+                });
+                return;
+            }
+            let editMoneyPayload = {
+                name: chatSplit[0],
+                level: chatSplit[1],
+                money: chatSplit[2]
+            };
+            try {
+                editMoneyPayload.money = Number.parseInt(editMoneyPayload.money);
+            }
+            catch (e) {
+                console.dir(e);
+                chatEvent.emit('send', {
+                    channelId,
+                    type: 'chat',
+                    data: "잘못 입력하셨습니다.",
+                    client
+                });
+                return;
+            }
+
+            response = await axios.put(`${apiEndpoint}/boss/money`, editMoneyPayload);
+            if (response.status != 200) {
+                return;
+            }
+            responseData = _.get(response, "data");
+            errorMessage = _.get(responseData, 'payload.message');
+            if (errorMessage) {
+                chatEvent.emit('send', {
+                    channelId,
+                    type: 'chat',
+                    data: errorMessage,
+                    client
+                });
+                return;
+            }
+
+            chatEvent.emit('send', {
+                channelId,
+                type: 'chat',
+                data: '수정 완료',
+                client
+            });
+            return;
+            break;
+        case 'insertReward':
+            if (chatSplit.length != 3) {
+                chatEvent.emit('send', {
+                    channelId,
+                    type: 'chat',
+                    data: "잘못 입력하셨습니다.",
+                    client
+                });
+                return;
+            }
+            let insertRewardPayload = {
+                name: chatSplit[0],
+                level: chatSplit[1],
+                reward: chatSplit[2]
+            };
+            try {
+                insertRewardPayload.reward = JSON.parse(insertRewardPayload.reward);
+            }
+            catch (e) {
+                // console.dir(e);
+            }
+
+            response = await axios.put(`${apiEndpoint}/boss/reward`, insertRewardPayload);
+            if (response.status != 200) {
+                return;
+            }
+            responseData = _.get(response, "data");
+            errorMessage = _.get(responseData, 'payload.message');
+            if (errorMessage) {
+                chatEvent.emit('send', {
+                    channelId,
+                    type: 'chat',
+                    data: errorMessage,
+                    client
+                });
+                return;
+            }
+
+            chatEvent.emit('send', {
+                channelId,
+                type: 'chat',
+                data: '추가 완료',
+                client
+            });
+            return;
+            break;
+        case 'editReward':
+            if (chatSplit.length != 4) {
+                chatEvent.emit('send', {
+                    channelId,
+                    type: 'chat',
+                    data: "잘못 입력하셨습니다.",
+                    client
+                });
+                return;
+            }
+            let editRewardPayload = {
+                name: chatSplit[0],
+                level: chatSplit[1],
+                prev: chatSplit[2],
+                curr: chatSplit[3]
+            };
+            try {
+                editRewardPayload.prev = JSON.parse(editRewardPayload.prev);
+                editRewardPayload.curr = JSON.parse(editRewardPayload.curr);
+            }
+            catch (e) {
+                // console.dir(e);
+            }
+
+            response = await axios.put(`${apiEndpoint}/boss/reward`, editRewardPayload);
+            if (response.status != 200) {
+                return;
+            }
+            responseData = _.get(response, "data");
+            errorMessage = _.get(responseData, 'payload.message');
+            if (errorMessage) {
+                chatEvent.emit('send', {
+                    channelId,
+                    type: 'chat',
+                    data: errorMessage,
+                    client
+                });
+                return;
+            }
+
+            chatEvent.emit('send', {
+                channelId,
+                type: 'chat',
+                data: '수정 완료',
+                client
+            });
+            return;
+            break;
+        // case 'deleteReward':
+        //     if (chatSplit.length != 3) {
+        //         chatEvent.emit('send', {
+        //             channelId,
+        //             type: 'chat',
+        //             data: "잘못 입력하셨습니다.",
+        //             client
+        //         });
+        //         return;
+        //     }
+
+        //     let game = chatSplit[0];
+        //     let name = chatSplit[1];
+
+        //     try {
+        //         deleteRewardPayload.reward = JSON.parse(deleteRewardPayload.reward);
+        //     }
+        //     catch (e) {
+        //         // console.dir(e);
+        //     }
+
+        //     response = await axios.put(`${apiEndpoint}/boss/reward`, deleteRewardPayload);
+        //     if (response.status != 200) {
+        //         return;
+        //     }
+        //     responseData = _.get(response, "data");
+        //     errorMessage = _.get(responseData, 'payload.message');
+        //     if (errorMessage) {
+        //         chatEvent.emit('send', {
+        //             channelId,
+        //             type: 'chat',
+        //             data: errorMessage,
+        //             client
+        //         });
+        //         return;
+        //     }
+
+        //     chatEvent.emit('send', {
+        //         channelId,
+        //         type: 'chat',
+        //         data: '추가 완료',
+        //         client
+        //     });
+        //     return;
+        //     break;
+        default:
+            break;
     }
 }
 

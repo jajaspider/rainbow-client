683202b8aa40f77e0e092df8f200c701c686c744
SonaAPI <jaja_spider@naver.com>
2023-02-12 14:41:54 +0900

    feat: add kakaolink

--- src/client/kakao-remote.js
+++ src/client/kakao-remote.js
@@ -1,33 +1,28 @@
-const _ = require('lodash');
-const {
-    Server,
-    Message
-} = require('@remote-kakao/core');
+const _ = require("lodash");
+const { Server, Message } = require("@remote-kakao/core");
 const crypto = require("crypto");
 // const KakaoLink = require('kakao-link');
 // const KakaoLink = require('../utils/kakaoLink');
-const path = require('path');
-const yaml = require('js-yaml');
-const fs = require('fs');
+const path = require("path");
+const yaml = require("js-yaml");
+const fs = require("fs");
 
 let publishQueue = [];
 let publishTime = new Date().getTime();
 
-const {
-    chatEvent
-} = require('../core/eventBridge/index');
+const { chatEvent } = require("../core/eventBridge/index");
 
-let configPath = path.join(process.cwd(), 'config', 'rainbow.develop.yaml');
+let configPath = path.join(process.cwd(), "config", "rainbow.develop.yaml");
 let config = yaml.load(fs.readFileSync(configPath));
 
 class KAKAOCLIENT {
-    constructor() {
-        this.server = new Server();
-        this.socket = _.get(this.server, 'socket');
-        // this.kakaoLink = null;
+  constructor() {
+    this.server = new Server();
+    this.socket = _.get(this.server, "socket");
+    // this.kakaoLink = null;
 
-        this.server.on('message', async (data) => {
-            /*
+    this.server.on("message", async (data) => {
+      /*
             {
               channelId: '947436800941629510',
               userId: '404997570721480704',
@@ -36,184 +31,212 @@ class KAKAOCLIENT {
               client: 'discord'
             }
             */
+      console.dir(data);
 
-            let channelId = _.get(data, 'room');
-            let userId = _.get(data, 'sender.name');
-            let nickname = _.get(data, 'sender.name');
-            let chat = _.get(data, 'content');
-            let senderInfo = {
-                address: _.get(data, 'remoteInfo.address'),
-                port: _.get(data, 'remoteInfo.port'),
-                room: _.get(data, 'room'),
-            }
+      let channelId = _.get(data, "room");
+      let userId = _.get(data, "sender.name");
+      let nickname = _.get(data, "sender.name");
+      let chat = _.get(data, "content");
+      let senderInfo = {
+        address: _.get(data, "remoteInfo.address"),
+        port: _.get(data, "remoteInfo.port"),
+        room: _.get(data, "room"),
+      };
 
-            if (!_.get(data, 'isGroupChat')) {
-                chatEvent.emit('send', {
-                    channelId,
-                    type: 'chat',
-                    data: "사용불가",
-                    senderInfo,
-                    client: 'kakao-remote'
-                });
-                return;
-            }
-
-            const user = {
-                channelId,
-                userId,
-                nickname,
-                chat,
-                client: 'kakao-remote',
-                senderInfo
-            };
-
-            chatEvent.emit('receive', user);
+      if (!_.get(data, "isGroupChat")) {
+        chatEvent.emit("send", {
+          channelId,
+          type: "chat",
+          data: "사용불가",
+          senderInfo,
+          client: "kakao-remote",
         });
+        return;
+      }
 
-        this.server.on('sendMessage', (payload) => {
-            console.dir({
-                method: 'kakao-remote, sendMessage',
-                payload
-            }, {
-                depth: null
-            });
+      const user = {
+        channelId,
+        userId,
+        nickname,
+        chat,
+        client: "kakao-remote",
+        senderInfo,
+      };
 
-            let senderInfo = _.get(payload, 'senderInfo');
+      chatEvent.emit("receive", user);
+    });
 
-            let address = _.get(senderInfo, 'address');
-            let port = _.get(senderInfo, 'port');
-            let room = _.get(senderInfo, 'room');
+    this.server.on("sendMessage", (payload) => {
+      console.dir(
+        {
+          method: "kakao-remote, sendMessage",
+          payload,
+        },
+        {
+          depth: null,
+        }
+      );
 
-            let message = _.get(payload, 'message');
+      let senderInfo = _.get(payload, "senderInfo");
 
-            const session = (0, crypto.randomUUID)();
-            const data = encodeURIComponent(JSON.stringify({
-                event: 'sendText',
-                data: {
-                    room,
-                    text: message
-                },
-                session
-            }));
-            this.socket.send(data, 0, data.length, port, address);
-        });
+      let address = _.get(senderInfo, "address");
+      let port = _.get(senderInfo, "port");
+      let room = _.get(senderInfo, "room");
 
-        // 현재 카링 안됨
-        this.server.on('sendKakaolink', async (payload) => {
-            console.dir(payload, { depth: null });
+      let message = _.get(payload, "message");
 
-            // // 2021.02 .06
-            // // 카링 재로그인 추가
-            // if (this.kakaoLinkDate != new Date().getDate()) {
-            //     await this.kakaoLogin();
-            // }
-
-            // let channelId = _.get(payload, 'channelId');
-
-            // // payload = _.get(payload, 'data');
-            // let templateId = _.get(payload, 'data.templateId');
-            // let templateArgs = _.get(payload, 'data.templateArgs');
-
-            // await this.kakaoLink.send(
-            //     `${channelId}`, {
-            //     link_ver: '4.0',
-            //     template_id: templateId,
-            //     template_args: templateArgs
-            // }, 'custom');
-
-            // let next = _.get(payload, 'next');
-            // if (next != null) {
-            //     chatEvent.emit('send', {
-            //         channelId,
-            //         type: 'chat',
-            //         data: next,
-            //         client
-            //     });
-            // }
-        });
-
-        this.server.on('ready', (port) => {
-            console.log(`server listen port:${port}`);
+      const session = (0, crypto.randomUUID)();
+      const data = encodeURIComponent(
+        JSON.stringify({
+          event: "sendText",
+          data: {
+            room,
+            text: message,
+          },
+          session,
         })
-    }
+      );
+      this.socket.send(data, 0, data.length, port, address);
+    });
 
-    async init() {
-        this.server.start(30004);
-        // await this.kakaoLogin();
-    }
+    // 현재 카링 안됨
+    this.server.on("sendKakaolink", async (payload) => {
+      console.dir(payload, { depth: null });
 
-    // async kakaoLogin() {
-    //     let javascriptKey = _.get(config, 'kakaoLink.javascriptKey');
-    //     let domain = _.get(config, 'kakaoLink.domain');
-    //     let email = _.get(config, 'kakaoLink.email');
-    //     let password = _.get(config, 'kakaoLink.password');
-    //     console.dir({
-    //         javascriptKey, domain, email, password
-    //     })
+      let senderInfo = _.get(payload, "senderInfo");
 
-    //     try {
-    //         this.kakaoLink = new KakaoLink(javascriptKey, domain);
-    //         await this.kakaoLink.login(email, password);
-    //         this.kakaoLinkDate = new Date().getDate();
-    //     } catch (e) {
-    //         console.dir(e);
-    //         console.dir('로그인 실패');
-    //     }
-    // }
+      let address = _.get(senderInfo, "address");
+      let port = _.get(senderInfo, "port");
+      let room = _.get(senderInfo, "room");
+      console.dir(payload.originPayload.data);
+
+      const session = (0, crypto.randomUUID)();
+      const data = encodeURIComponent(
+        JSON.stringify({
+          event: "sendKakaolink",
+          data: {
+            room,
+            payload: JSON.stringify(payload.originPayload.data),
+          },
+          session,
+        })
+      );
+      this.socket.send(data, 0, data.length, port, address);
+
+      // // 2021.02 .06
+      // // 카링 재로그인 추가
+      // if (this.kakaoLinkDate != new Date().getDate()) {
+      //     await this.kakaoLogin();
+      // }
+
+      // let channelId = _.get(payload, 'channelId');
+
+      // // payload = _.get(payload, 'data');
+      // let templateId = _.get(payload, 'data.templateId');
+      // let templateArgs = _.get(payload, 'data.templateArgs');
+
+      // await this.kakaoLink.send(
+      //     `${channelId}`, {
+      //     link_ver: '4.0',
+      //     template_id: templateId,
+      //     template_args: templateArgs
+      // }, 'custom');
+
+      // let next = _.get(payload, 'next');
+      // if (next != null) {
+      //     chatEvent.emit('send', {
+      //         channelId,
+      //         type: 'chat',
+      //         data: next,
+      //         client
+      //     });
+      // }
+    });
+
+    this.server.on("ready", (port) => {
+      console.log(`server listen port:${port}`);
+    });
+  }
+
+  async init() {
+    this.server.start(30004);
+    // await this.kakaoLogin();
+  }
+
+  // async kakaoLogin() {
+  //     let javascriptKey = _.get(config, 'kakaoLink.javascriptKey');
+  //     let domain = _.get(config, 'kakaoLink.domain');
+  //     let email = _.get(config, 'kakaoLink.email');
+  //     let password = _.get(config, 'kakaoLink.password');
+  //     console.dir({
+  //         javascriptKey, domain, email, password
+  //     })
+
+  //     try {
+  //         this.kakaoLink = new KakaoLink(javascriptKey, domain);
+  //         await this.kakaoLink.login(email, password);
+  //         this.kakaoLinkDate = new Date().getDate();
+  //     } catch (e) {
+  //         console.dir(e);
+  //         console.dir('로그인 실패');
+  //     }
+  // }
 }
 
-chatEvent.on('send', (payload) => {
-    let client = _.get(payload, 'client');
-    //kakao-remote가 아니면 처리할 필요없음
-    if (client != 'kakao-remote') {
-        return;
-    }
+chatEvent.on("send", (payload) => {
+  let client = _.get(payload, "client");
+  //kakao-remote가 아니면 처리할 필요없음
+  if (client != "kakao-remote") {
+    return;
+  }
 
-    publishQueue.push(payload);
+  publishQueue.push(payload);
 });
 
 function queueManager() {
-    if (!_.isEmpty(publishQueue)) {
-        //큐에있는 데이터를 가져옴
-        let originPayload = publishQueue.shift();
+  if (!_.isEmpty(publishQueue)) {
+    //큐에있는 데이터를 가져옴
+    let originPayload = publishQueue.shift();
 
-        //그리고 해당 방에다가 마지막으로 publish한 시간을 체크
-        let channelId = _.get(originPayload, 'channelId');
+    //그리고 해당 방에다가 마지막으로 publish한 시간을 체크
+    let channelId = _.get(originPayload, "channelId");
 
-        //기존에 퍼블리시한 시간과 1초이상 차이나는지 확인
-        if ((new Date().getTime() - publishTime) >= 1000) {
-            const type = _.get(originPayload, 'type');
-            // 일반 str형태 chat
-            if (type === 'chat') {
-                let senderInfo = _.get(originPayload, 'senderInfo');
-                let message = _.get(originPayload, 'data');
+    //기존에 퍼블리시한 시간과 1초이상 차이나는지 확인
+    if (new Date().getTime() - publishTime >= 1000) {
+      const type = _.get(originPayload, "type");
+      // 일반 str형태 chat
+      if (type === "chat") {
+        let senderInfo = _.get(originPayload, "senderInfo");
+        let message = _.get(originPayload, "data");
 
-                kakaoClient.server.emit('sendMessage', {
-                    senderInfo,
-                    message
-                });
-            }
-            else if (type === 'kakaolink') {
-                kakaoClient.server.emit('sendKakaolink', {
-                    originPayload
-                })
-            }
+        kakaoClient.server.emit("sendMessage", {
+          senderInfo,
+          message,
+        });
+      } else if (type === "kakaolink") {
+        let senderInfo = _.get(originPayload, "senderInfo");
 
-            //마지막으로 publish한 시간 기록
-            // _.set(publishManager, channelId, {
-            //     date: new Date().getTime()
-            // });
-            publishTime = new Date().getTime();
-        }
-        // 차이가 안난다면
-        else {
-            publishQueue.push(originPayload);
-        }
+        kakaoClient.server.emit("sendKakaolink", {
+          senderInfo,
+          originPayload,
+        });
+      }
+
+      //마지막으로 publish한 시간 기록
+      // _.set(publishManager, channelId, {
+      //     date: new Date().getTime()
+      // });
+      publishTime = new Date().getTime();
     }
+    // 차이가 안난다면
+    else {
+      publishQueue.push(originPayload);
+    }
+  }
 
-    setTimeout(() => {
-        queueManager();
-    }, 300);
+  setTimeout(() => {
+    queueManager();
+  }, 300);
 }
 
 queueManager();
